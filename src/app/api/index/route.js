7b0a9453a62692e16c25b8ec2c1216f4bbf44601
inexperienced-developer <inexperienced.developer.games@gmail.com>
2023-11-13 21:22:59 -0500

    API test 4

--- src/app/api/index/route.js
+++ src/app/api/index/route.js
@@ -1,5 +1,11 @@
 import { NextResponse } from 'next/server';
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+const openai = new OpenAIApi(configuration);
 
 export async function GET() {
-    return NextResponse.json({ ok: true });
+    return NextResponse.json({ ok: process.env.OPENAI_API_KEY });
   }
