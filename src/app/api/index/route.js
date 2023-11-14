a4ecaf6f0aed86fad776e2e586c7157f68c4e2ec
inexperienced-developer <inexperienced.developer.games@gmail.com>
2023-11-13 21:32:51 -0500

    API test lol

--- src/app/api/index/route.js
+++ src/app/api/index/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import OpenAI from "openai";
 
-const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
+const openai = new OpenAI();
 
 export async function GET() {
     const completion = await openai.chat.completions.create({
