5b019deabe59d8bb4784fdb7b71e5ba2771ba677
inexperienced-developer <inexperienced.developer.games@gmail.com>
2023-11-13 21:17:36 -0500

    API test 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       setSubmitted(true);
   
       try {
-        const response = await fetch('/api/main.py', {
+        const response = await fetch('/api/index', {
           // const response = await fetch('https://us-east4-navytrainingtest.cloudfunctions.net/ask-9c-result', {
           method: 'POST',
           headers: {
